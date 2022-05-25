959f426bcaf4284fb809a5fc35918983ac8a6cce
Luizrodgs <luizrodgs1@gmail.com>
2022-05-25 19:48:21 -0300

    Separa partidas em dics

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-with open('Quake.txt') as log:
+filename = 'Quake.txt'
+
+game = 0
+
+jogo = dict()
+
+with open(filename) as log:
     for line in log:
-        print(line)
+        if 'InitGame' in line:
+            game += 1
+            jogo[game] = dict()
+        else:
+            a = line.strip().split(sep=' ')
+            break
+
+print(a)
